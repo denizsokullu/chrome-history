9975099a18a38709e107bc113dd47da4fc9f3bca
denizsokullu <dsokullu@andrew.cmu.edu>
2018-05-05 17:28:04 -0400

    fixed build server

--- scripts/serve.js
+++ scripts/serve.js
@@ -9,7 +9,7 @@ app.get('*',(req,res)=>{
   res.sendFile(path.join(__dirname,'../build/index.html'));
 })
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 console.log(`Listening at ${port}`);
 app.listen(port);
